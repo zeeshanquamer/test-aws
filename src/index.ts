b8a936ad4083e188570539c4b2004acd8b1fa5c2
zeeshanquamer <zeesh654@gmail.com>
2026-01-31 22:07:31 +0530

    testing

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Testing.");
+  res.send("Hello, World!");
 });
 
 const PORT = process.env.PORT || 5000;
