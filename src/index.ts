424e8c603809979a680b1c6a758e0eabaf261ea1
zeeshanquamer <zeesh654@gmail.com>
2026-01-31 18:18:13 +0530

    added: pipeline

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello, World! Testing3000");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
