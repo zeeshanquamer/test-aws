ea4d9504445e966c4264b92c80a6c880f1ff7736
zeeshanquamer <zeesh654@gmail.com>
2026-01-31 18:24:38 +0530

    added: pipeline

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Testing deployment.");
+  res.send("Hello, World! Testing.");
 });
 
 const PORT = process.env.PORT || 5000;
